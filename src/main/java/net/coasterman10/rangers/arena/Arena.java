0e0913d9bdb6d3ba443065d77066f68c9625cc51
coasterman10 <christian.s.farrell@gmail.com>
2014-10-25 00:41:35 -0500

    Fixed configuration issues

--- src/main/java/net/coasterman10/rangers/arena/Arena.java
+++ src/main/java/net/coasterman10/rangers/arena/Arena.java
@@ -113,8 +113,8 @@ public class Arena {
         for (GameTeam team : GameTeam.values()) {
             SpawnVector spawn = spawns.get(team) != null ? new SpawnVector(spawns.get(team)) : null;
             Vector chest = chests.get(team) != null ? chests.get(team).toVector() : null;
-            ConfigUtil.setVector(conf, "spawns." + team.name(), spawn);
-            ConfigUtil.setVector(conf, "chests." + team.name(), chest);
+            ConfigUtil.setVector(conf, "spawns." + team.name().toLowerCase(), spawn);
+            ConfigUtil.setVector(conf, "chests." + team.name().toLowerCase(), chest);
         }
 
         config.save();
